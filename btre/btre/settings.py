836965d0980fe058a44af675b466162e4caf2fe7
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-25 14:57:56 -0800

    added partial directory and footer, topbar, and navbar html files

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = 'static/'
-STATICFILES_DIR =[
+STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'btre/static')
 ]
 
